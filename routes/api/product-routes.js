d4cf0fcd88ed38990550b143fa4a4f62655fd38c
George Haworth <ghaworth1@gmail.com>
2023-08-31 13:04:17 -0500

    fixed routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,50 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include:[
+      {
+        model: Category,
+        attributes: ['category_name']
+      }
+    ]
+  })
+  .then(ProductData => res.json(ProductData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+  });
 });
+
+
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+    // find a single product by its `id` 
+    Product.findOne({
+      where:{
+        id: req.params.id
+      },
+      attributes: ['product_name', 'price', 'stock'],
+      include:[
+        {
+          model: Category,
+          attributes: ['category_name']
+        }
+      ]
+    })
+    .then(ProductData => {
+      if (!ProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(ProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -94,6 +131,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(ProductData => {
+    if (!ProductData){
+      res.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(ProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
